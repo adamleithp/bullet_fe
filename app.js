c60fd0d60555b1f6347a31963dbd6bc798cf412c
adam-jomedia <adam.leith.pietrantonio@jomediainc.com>
2019-03-09 18:08:58 -0400

    Trying to get this working...

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const serveStatic = require("serve-static")
-const path = require('path');
+const serveStatic = require('serve-static');
 
-app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT || 9090;
-app.listen(port);
+let app = express();
+app.use(serveStatic(__dirname + "/dist"));
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log('Listening on port ' + port)
+});
