deba098095f6570152fa6799c50f1f7644b4a9ae
adam-jomedia <adam.leith.pietrantonio@jomediainc.com>
2019-03-09 18:11:48 -0400

    Trying to get this working...

---
+++ dist/precache-manifest.b93f9ae5169e77c8a364f50065070032.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "dafed3cc861fcf2e29ed",
+    "url": "/js/chunk-vendors.0e0eca66.js"
+  },
+  {
+    "revision": "5064db2073b74f01a52f",
+    "url": "/js/app.8253325c.js"
+  },
+  {
+    "revision": "7d705f9a1a77336ed921",
+    "url": "/js/about.2b6705d6.js"
+  },
+  {
+    "revision": "d3f5f363f3c218cc7f9941d75ab3809e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "5064db2073b74f01a52f",
+    "url": "/css/app.84fe17e1.css"
+  }
+];
