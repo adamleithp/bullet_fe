98573a975f38625da9fb21256ab82ebadb007f88
adam-jomedia <adam.leith.pietrantonio@jomediainc.com>
2019-03-09 17:56:17 -0400

    Added Start command

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
-var express = require('express');
-var port = process.env.PORT || 3000;
-var app = express();
+const express = require('express');
+const serveStatic = require("serve-static")
+const path = require('path');
 
-app.get('/', function (req, res) {
-	res.sendFile(path.join(__dirname, 'dist/index.html'));
-});
-app.listen(port, function () {
-	console.log(`Example app listening on port !`);
-});
+app = express();
+app.use(serveStatic(path.join(__dirname, 'dist')));
+const port = process.env.PORT || 80;
+app.listen(port);
