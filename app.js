726e601322532ffdf314faaa5b36e2044328f0d4
adam-jomedia <adam.leith.pietrantonio@jomediainc.com>
2019-03-13 21:52:33 -0400

    General Navigation added

--- app.js
+++ app.js
@@ -6,5 +6,5 @@ app.use(serveStatic(__dirname + "/dist"));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log('Listening on port ' + port)
+  // console.log('Listening on port ' + port)
 });
